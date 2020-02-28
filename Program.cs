d4833aaf86d17096f9a856e54cad5d956941274d
macgibbons <macgibbons@gmail.com>
2020-02-28 09:41:34 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListIntro {
+    class Program {
+        static void Main (string[] args) {
+            List<string> nssCohorts = new List<string> {
+                "Cohort 36",
+                "Cohort 37",
+                "Cohort 38",
+                "Cohort 39"
+            };
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
