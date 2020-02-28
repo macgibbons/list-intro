3c2bdf63d2f7aaa1ab9285f13955ebf500d3e4a2
macgibbons <macgibbons@gmail.com>
2020-02-28 09:59:09 -0600

    practice foreach loops as well as .add and .remove

--- Program.cs
+++ Program.cs
@@ -1,16 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ListIntro {
-    class Program {
-        static void Main (string[] args) {
-            List<string> nssCohorts = new List<string> {
+namespace ListIntro
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            List<string> nssCohorts = new List<string>
+            {
                 "Cohort 36",
                 "Cohort 37",
                 "Cohort 38",
                 "Cohort 39"
             };
-            Console.WriteLine ("Hello World!");
+
+            nssCohorts.Add ("Cohort 40");
+
+            List<int> birthYears = new List<int>
+            {
+                1996,
+                1992,
+                1997,
+                2001,
+            };
+
+            birthYears.Add (1990);
+
+            foreach (string cohort in nssCohorts)
+            {
+                Console.WriteLine (cohort);
+            }
+
+            birthYears.Remove (1997);
+
+            foreach (int year in birthYears)
+            {
+                Console.WriteLine (year);
+
+                if (year == 1996)
+                {
+                    Console.WriteLine ("go bulls! 🏀");
+                }
+            }
+
         }
     }
 }
